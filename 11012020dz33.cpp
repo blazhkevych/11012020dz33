332ffd335b0505a98511c0bf299cb0f614ef7b38
blazhkevych <65856963+blazhkevych@users.noreply.github.com>
2020-07-19 18:44:43 +0300

    Добавьте файлы проекта.

---
+++ 11012020dz33.cpp
@@ -0,0 +1,16 @@
+﻿/*
+* Пользователь 	вводит с клавиатуры расстояние в милях.Перевести его в метры.
+*/
+#include <iostream>
+#include <Windows.h>;
+using namespace std;
+int main()
+{
+	SetConsoleOutputCP(1251);
+	cout << "Введите мили : \n";
+	double a, b;
+	cin >> a;
+	b = a * 1.609;
+	cout << "км. = " << b << endl;
+	return 0;
+}
